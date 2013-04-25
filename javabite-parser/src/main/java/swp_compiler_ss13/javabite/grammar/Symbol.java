8f2de6a244d57055fe5e80041f37b8d306ade132
Tilman Krauß <Tilman.krauss@gmail.com>
2013-04-25 23:29:59 +0200

    Added Grammar definition to prepare the environment for SLR-parsing

---
+++ javabite-parser/src/main/java/swp_compiler_ss13/javabite/grammar/Symbol.java
@@ -0,0 +1,18 @@
+package swp_compiler_ss13.javabite.grammar;
+
+/**
+ * Just a marker interface for Symbols of an Alphabet
+ * @author Till
+ *
+ */
+public interface Symbol {
+
+}
+
+interface NonTerminal extends Symbol{
+	
+}
+
+interface Terminal extends Symbol{
+	
+}
