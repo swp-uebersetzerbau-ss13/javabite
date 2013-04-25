f57055af8b125723c3b8eac4663d2c40b662d804
Ahmet-Serdar Karakaya <ahmetserdar@hotmail.de>
2013-04-25 16:25:31 +0200

    added skeleton implementation for parser

---
+++ javabite-parser/src/main/java/swp_compiler_ss13/javabite/parser/JavabiteParser.java
@@ -0,0 +1,12 @@
+package swp_compiler_ss13.javabite.parser;
+
+import swp_compiler_ss13.common.ast.AST;
+import swp_compiler_ss13.common.parser.Parser;
+
+public class JavabiteParser implements Parser {
+
+	@Override
+	public AST getParsedAST() {
+		return null;
+	}
+}
