a57de1423e3e3700b42f768b7a61156b8e41d819
flow85 <mercksflorian@hotmail.com>
2013-04-25 17:08:29 +0200

    add skeleton implementation for intermediate code generator

---
+++ javabite-code-gen/src/main/java/swp_compiler_ss13/javabite/codegen/JavaBiteCodeGenerator.java
@@ -0,0 +1,19 @@
+package swp_compiler_ss13.javabite.codegen;
+
+import java.util.List;
+
+import swp_compiler_ss13.common.ast.AST;
+import swp_compiler_ss13.common.backend.Quadruple;
+import swp_compiler_ss13.common.ir.IntermediateCodeGenerator;
+import swp_compiler_ss13.common.ir.IntermediateCodeGeneratorException;
+
+public class JavaBiteCodeGenerator implements IntermediateCodeGenerator {
+
+	@Override
+	public List<Quadruple> generateIntermediateCode(AST ast)
+			throws IntermediateCodeGeneratorException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
