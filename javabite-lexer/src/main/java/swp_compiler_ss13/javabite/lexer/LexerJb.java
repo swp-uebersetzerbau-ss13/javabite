3282228eff6296192da17552bf4e5bfe7935e893
Ferhat Beyaz <fb13@live.de>
2013-04-30 14:50:12 +0200

    #10 added initial lexer implementation

--- javabite-lexer/src/main/java/swp_compiler_ss13/javabite/lexer/LexerJb.java
+++ javabite-lexer/src/main/java/swp_compiler_ss13/javabite/lexer/LexerJb.java
@@ -1,22 +1,51 @@
 package swp_compiler_ss13.javabite.lexer;
 
 import java.io.InputStream;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import swp_compiler_ss13.common.lexer.Lexer;
 import swp_compiler_ss13.common.lexer.Token;
+import swp_compiler_ss13.common.lexer.TokenType;
+import swp_compiler_ss13.javabite.token.TokenJb;
 
 public class LexerJb implements Lexer {
-
+	
+	Pattern tokenPatterns;
+	Matcher matcher;
+	
+	LexerJb(InputStream stream) {
+		StringBuffer buffer = new StringBuffer();
+		for (TokenType tokenType : TokenType.values()) {
+			buffer.append(String.format("|(?<%s>%s)", tokenType.name(), ""));
+			// TODO: second parameter would still tokenType.regExpr, discussion in #9
+		}
+		
+		tokenPatterns = Pattern.compile(new String(buffer.substring(1)));
+		setSourceStream(stream);
+	}
+	
 	@Override
 	public void setSourceStream(InputStream stream) {
-		// TODO Auto-generated method stub
-		
+		if (stream != null) {
+			// extract string from stream
+			Scanner scanner = new Scanner(stream, "UTF-8");
+			String sourcecode = scanner.useDelimiter("\\A").next();
+			scanner.close();
+			
+			// matcher should work on the new string
+			matcher = tokenPatterns.matcher(sourcecode);
+		}
 	}
 
 	@Override
 	public Token getNextToken() {
-		// TODO Auto-generated method stub
+		if (matcher.find()) {
+			if (matcher.group(TokenType.NUM.name()) != null) {
+				return new TokenJb(TokenType.NUM, matcher.group(TokenType.NUM.name()));
+			}
+		}
 		return null;
 	}
-
 }
