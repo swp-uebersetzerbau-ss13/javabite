b9ed5a15a4a97d11f6ac49b40a931892fe51c7dc
dildik <ddurmaz88@googlemail.com>
2013-04-30 13:25:59 +0200

    #10 added template implementations of token and lexer

---
+++ javabite-common/src/main/java/swp_compiler_ss13/javabite/token/TokenJb.java
@@ -0,0 +1,38 @@
+package swp_compiler_ss13.javabite.token;
+
+import swp_compiler_ss13.common.lexer.Token;
+import swp_compiler_ss13.common.lexer.TokenType;
+
+public class TokenJb implements Token {
+	
+	public String value;
+	public TokenType type;
+	int line;
+	int column;
+	
+
+	@Override
+	public String getValue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TokenType getTokenType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer getLine() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer getColumn() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ javabite-lexer/src/main/java/swp_compiler_ss13/javabite/lexer/LexerJb.java
@@ -0,0 +1,22 @@
+package swp_compiler_ss13.javabite.lexer;
+
+import java.io.InputStream;
+
+import swp_compiler_ss13.common.lexer.Lexer;
+import swp_compiler_ss13.common.lexer.Token;
+
+public class LexerJb implements Lexer {
+
+	@Override
+	public void setSourceStream(InputStream stream) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Token getNextToken() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
