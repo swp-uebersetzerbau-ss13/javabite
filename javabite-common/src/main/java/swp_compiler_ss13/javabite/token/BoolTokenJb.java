febbfa7bf82b17bc42782e302d298a49f8b96aca
dildik <ddurmaz88@googlemail.com>
2013-04-30 14:14:34 +0200

    #10 adapted bool token implementation

--- javabite-common/src/main/java/swp_compiler_ss13/javabite/token/BoolTokenJb.java
+++ javabite-common/src/main/java/swp_compiler_ss13/javabite/token/BoolTokenJb.java
@@ -3,36 +3,31 @@ package swp_compiler_ss13.javabite.token;
 import swp_compiler_ss13.common.lexer.BoolToken;
 import swp_compiler_ss13.common.lexer.TokenType;
 
-public class BoolTokenJb implements BoolToken {
+public class BoolTokenJb extends TokenJb implements BoolToken {
 
 	@Override
 	public String getValue() {
-		// TODO Auto-generated method stub
-		return null;
+		return super.getValue();
 	}
 
 	@Override
 	public TokenType getTokenType() {
-		// TODO Auto-generated method stub
-		return null;
+		return super.getTokenType();
 	}
 
 	@Override
 	public Integer getLine() {
-		// TODO Auto-generated method stub
-		return null;
+		return super.getLine();
 	}
 
 	@Override
 	public Integer getColumn() {
-		// TODO Auto-generated method stub
-		return null;
+		return super.getColumn();
 	}
 
 	@Override
 	public Boolean getBooleanValue() {
-		// TODO Auto-generated method stub
-		return null;
+		return Boolean.parseBoolean(getValue());
 	}
 
 }
