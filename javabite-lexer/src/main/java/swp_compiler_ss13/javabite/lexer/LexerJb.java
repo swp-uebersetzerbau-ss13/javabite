d7406757fcc3e45ea812321743cff606065c0550
dildik <ddurmaz88@gmail.com>
2013-05-01 18:32:25 +0200

    #10 fixed a bug in the NUM and REAL regex

--- javabite-lexer/src/main/java/swp_compiler_ss13/javabite/lexer/LexerJb.java
+++ javabite-lexer/src/main/java/swp_compiler_ss13/javabite/lexer/LexerJb.java
@@ -21,10 +21,10 @@ public class LexerJb implements Lexer {
 		for (TokenType tokenType : TokenType.values()) {
 			switch (tokenType.name()) {
 				case "NUM":
-					buffer.append(String.format("|(?<%s>%s)", tokenType.name(), "[0-9]+ ((E|e)-?[0-9+])?"));
+					buffer.append(String.format("|(?<%s>%s)", tokenType.name(), "[0-9]+((E|e)-?[0-9+])?"));
 					break;
 				case "REAL":
-					buffer.append(String.format("|(?<%s>%s)", tokenType.name(), "[0-9]+.[0-9]+ ((E|e)-?[0-9+])?"));
+					buffer.append(String.format("|(?<%s>%s)", tokenType.name(), "[0-9]+.[0-9]+((E|e)-?[0-9+])?"));
 					break;
 				case "TRUE":
 					buffer.append(String.format("|(?<%s>%s)", tokenType.name(), "true"));
@@ -36,7 +36,7 @@ public class LexerJb implements Lexer {
 			// TODO: see discussion in #9
 		}
 		
-		System.out.println("Patter: " + buffer);
+		System.out.println("Pattern: " + buffer);
 		tokenPatterns = Pattern.compile(new String(buffer.substring(1)));
 		setSourceStream(stream);
 	}
@@ -58,21 +58,30 @@ public class LexerJb implements Lexer {
 	public Token getNextToken() {
 		if (matcher.find()) {
 			if (matcher.group(TokenType.NUM.name()) != null) {
+				System.out.println(matcher.group() + " @ " + matcher.start() + ", " + matcher.end());
 				return new TokenJb(TokenType.NUM, matcher.group(TokenType.NUM.name()));
+			}
+			if (matcher.group(TokenType.TRUE.name()) != null) {
+				System.out.println(matcher.group() + " @ " + matcher.start() + ", " + matcher.end());
+				return new TokenJb(TokenType.TRUE, matcher.group(TokenType.TRUE.name()));
+			}
+			if (matcher.group(TokenType.FALSE.name()) != null) {
+				System.out.println(matcher.group() + " @ " + matcher.start() + ", " + matcher.end());
+				return new TokenJb(TokenType.FALSE, matcher.group(TokenType.FALSE.name()));
 			}
 		}
 		return null;
 	}
 	
 	public static void main(String[] args) {
-		String myString = "4 false 3 true";
+		String myString = "4 false 3 true 55";
+		System.out.println("Input: " + myString);
 		byte[] bytes = myString.getBytes();
 		InputStream is = new ByteArrayInputStream(bytes);
 		if (is != null) {
 			LexerJb lexer = new LexerJb(is);
 			Token t = lexer.getNextToken();
 			while (t != null) {
-				System.out.println(t.getValue());
 				t = lexer.getNextToken();
 			}
 		}
