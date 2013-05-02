3423417e59157469bc96863067b4ab5e5acb67f3
Ferhat Beyaz <fb13@live.de>
2013-05-02 17:23:52 +0200

    initial commit for javabite code generation

--- javabite-code-gen/src/main/java/swp_compiler_ss13/javabite/codegen/JavaBiteCodeGenerator.java
+++ javabite-code-gen/src/main/java/swp_compiler_ss13/javabite/codegen/JavaBiteCodeGenerator.java
@@ -1,19 +1,28 @@
 package swp_compiler_ss13.javabite.codegen;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import swp_compiler_ss13.common.ast.AST;
+import swp_compiler_ss13.common.ast.ASTNode;
 import swp_compiler_ss13.common.backend.Quadruple;
 import swp_compiler_ss13.common.ir.IntermediateCodeGenerator;
 import swp_compiler_ss13.common.ir.IntermediateCodeGeneratorException;
 
 public class JavaBiteCodeGenerator implements IntermediateCodeGenerator {
-
-	@Override
-	public List<Quadruple> generateIntermediateCode(AST ast)
-			throws IntermediateCodeGeneratorException {
-		// TODO Auto-generated method stub
-		return null;
+	
+	List<Quadruple> quadruples;
+	
+	JavaBiteCodeGenerator() {
+		quadruples = new ArrayList<Quadruple>();
 	}
-
+	
+	@Override
+	public List<Quadruple> generateIntermediateCode(AST ast) throws IntermediateCodeGeneratorException {
+		
+		//TODO: iterate over AST and add quadruples to quadruple list
+		ASTNode node = ast.getRootNode();
+		
+		return quadruples;
+	}
 }
