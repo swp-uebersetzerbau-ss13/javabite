5cca1210f72bd03243e693610e81d0817a2cbcc7
Ferhat Beyaz <fb13@live.de>
2013-05-18 19:53:39 +0200

    progressbar now is in toolbar and inserted play button

--- javabite-compiler/src/main/java/swp_compiler_ss13/javabite/gui/MainFrame.java
+++ javabite-compiler/src/main/java/swp_compiler_ss13/javabite/gui/MainFrame.java
@@ -1,59 +1,38 @@
 package swp_compiler_ss13.javabite.gui;
 
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.text.DefaultStyledDocument;
-import javax.swing.text.StyleConstants;
-import javax.swing.text.StyledDocument;
-
-import java.awt.Component;
-import javax.swing.Box;
-import javax.swing.JComponent;
-import javax.swing.JScrollPane;
-import javax.swing.JToolBar;
-import javax.swing.JMenuBar;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
-import javax.swing.JButton;
-import javax.swing.JSpinner;
-import javax.swing.JTabbedPane;
-import javax.swing.JEditorPane;
-import javax.swing.JDesktopPane;
-import javax.swing.JProgressBar;
-import javax.swing.JSplitPane;
-import java.awt.FlowLayout;
-import javax.swing.JTextPane;
-import javax.swing.JScrollBar;
-import javax.swing.JInternalFrame;
-
 import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.ScrollPane;
-import java.awt.Scrollbar;
-import java.awt.Panel;
-import javax.swing.JTree;
-
+import java.awt.EventQueue;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.io.ByteArrayInputStream;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.JSplitPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextPane;
+import javax.swing.JToolBar;
+import javax.swing.border.EmptyBorder;
+import javax.swing.text.DefaultStyledDocument;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
 
 import swp_compiler_ss13.common.lexer.Token;
 import swp_compiler_ss13.common.lexer.TokenType;
 import swp_compiler_ss13.javabite.lexer.LexerJb;
+import java.awt.FlowLayout;
 
 public class MainFrame extends JFrame {
-	
+
 	private JPanel contentPane;
 	
 	//Class to setup styles for our sourcecode
@@ -87,7 +66,7 @@ public class MainFrame extends JFrame {
 	 * Create the frame.
 	 */
 	public MainFrame() {
-		setTitle("Javabite Compiler Pro 2013");
+		setTitle("Javabite Compiler");
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		getContentPane().setLayout(new BorderLayout(0, 0));
@@ -116,15 +95,20 @@ public class MainFrame extends JFrame {
 		JMenuItem mntmTac = new JMenuItem("TAC");
 		mnVisualisierung.add(mntmTac);
 		
-		JButton btnNewButton = new JButton("Ausf\u00FChren");
+		JButton btnNewButton = new JButton("\u25BA");
 		menuBar.add(btnNewButton);
-		
-		progressBar = new JProgressBar();
-		progressBar.setVisible(false);
-		menuBar.add(progressBar);
 		
 		JToolBar toolBar = new JToolBar();
 		getContentPane().add(toolBar, BorderLayout.SOUTH);
+		
+		JPanel panel_1 = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) panel_1.getLayout();
+		flowLayout.setAlignment(FlowLayout.RIGHT);
+		toolBar.add(panel_1);
+		
+		progressBar = new JProgressBar();
+		panel_1.add(progressBar);
+		progressBar.setVisible(false);
 		
 		JSplitPane splitPane = new JSplitPane();
 		splitPane.setOrientation(JSplitPane.VERTICAL_SPLIT);
@@ -141,7 +125,6 @@ public class MainFrame extends JFrame {
 		dtrpnSourcecode.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyTyped(KeyEvent e) {
-				
 				int index = 0;
 				String text = dtrpnSourcecode.getText();
 				List<Token> tokens = getTokenList(text);
@@ -158,9 +141,7 @@ public class MainFrame extends JFrame {
 					index = text.indexOf(current_token.getValue(), lastIndex);
 					lastIndex = index + current_token.getValue().length();
 					styleToken(current_token.getTokenType(), index , index + current_token.getValue().length());
-					
 				}
-
 			}
 		});
 		
@@ -176,65 +157,45 @@ public class MainFrame extends JFrame {
 		JTextPane textPane = new JTextPane();
 		textPane.setText("Logs");
 		logTabbedPane.addTab("Logs", null, textPane, null);
-		
-		// Events
-		btnNewButton.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				progressBar.setVisible(true);
-			}
-		});
-		
-		
 	}
 	
 	private List<Token> getTokenList(String text) {
 		Token t;
 		List<Token> tokens = new ArrayList<Token>();
+		
 		try {
-			lexer.setSourceStream(new ByteArrayInputStream(text
-					.getBytes("UTF-8")));
-
+			lexer.setSourceStream(new ByteArrayInputStream(text.getBytes("UTF-8")));
 			do {
 				t = lexer.getNextToken();
 				tokens.add(t);
-				
 			} while (t.getTokenType() != TokenType.EOF);
-			
 		} catch (UnsupportedEncodingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 		return tokens;
-
 	}
 
 	private void styleToken(TokenType tokenType, int start, int end) {
 		System.out.println(start + " bis " + end + ": " + tokenType);
 		if (tokenType == TokenType.STRING ) {
-
 			javax.swing.text.Style style = dtrpnSourcecode.addStyle("Blue", null);
 			StyleConstants.setForeground(style, Color.BLUE);
-			doc.setCharacterAttributes(start, end, dtrpnSourcecode.getStyle("Blue"),
-					true);
-			
+			doc.setCharacterAttributes(start, end, dtrpnSourcecode.getStyle("Blue"), true);
 		} else if(tokenType == TokenType.IF || tokenType == TokenType.WHILE || tokenType == TokenType.DO || tokenType == TokenType.BREAK || tokenType == TokenType.RETURN || tokenType == TokenType.PRINT ) {
 			javax.swing.text.Style style = dtrpnSourcecode.addStyle("Pink", null);
 			StyleConstants.setForeground(style, new Color(145, 0, 85));
-			doc.setCharacterAttributes(start, end, dtrpnSourcecode.getStyle("Pink"),
-					true);
+			doc.setCharacterAttributes(start, end, dtrpnSourcecode.getStyle("Pink"), true);
 			
 		} else if(tokenType == TokenType.COMMENT) {
 			javax.swing.text.Style style = dtrpnSourcecode.addStyle("Gray", null);
 			StyleConstants.setForeground(style, Color.GRAY);
-			doc.setCharacterAttributes(start, end, dtrpnSourcecode.getStyle("Gray"),
-					true);
+			doc.setCharacterAttributes(start, end, dtrpnSourcecode.getStyle("Gray"), true);
 		} else {
 			javax.swing.text.Style style = dtrpnSourcecode.addStyle("Black", null);
 			StyleConstants.setForeground(style, Color.BLACK);
-			doc.setCharacterAttributes(start, end, dtrpnSourcecode.getStyle("Black"),
-					true);
+			doc.setCharacterAttributes(start, end, dtrpnSourcecode.getStyle("Black"), true);
 		}
-
 	}
 }
