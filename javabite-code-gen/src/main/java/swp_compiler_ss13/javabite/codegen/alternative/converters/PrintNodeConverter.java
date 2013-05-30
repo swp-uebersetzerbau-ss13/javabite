7f775cc66c54858849c2bd09c01b213beb33cb0a
alpinster <alpinms@yahoo.de>
2013-05-30 16:27:20 +0200

    printnode erweitert

--- javabite-code-gen/src/main/java/swp_compiler_ss13/javabite/codegen/alternative/converters/PrintNodeConverter.java
+++ javabite-code-gen/src/main/java/swp_compiler_ss13/javabite/codegen/alternative/converters/PrintNodeConverter.java
@@ -8,9 +8,11 @@ import swp_compiler_ss13.common.ast.nodes.unary.PrintNode;
 import swp_compiler_ss13.common.backend.Quadruple;
 import swp_compiler_ss13.common.backend.Quadruple.Operator;
 import swp_compiler_ss13.common.ir.IntermediateCodeGeneratorException;
+import swp_compiler_ss13.common.types.Type;
 import swp_compiler_ss13.common.types.Type.Kind;
 import swp_compiler_ss13.javabite.codegen.alternative.CastingAst2CodeConverter;
 import swp_compiler_ss13.javabite.codegen.alternative.IdentifierData;
+import swp_compiler_ss13.javabite.codegen.alternative.QuadrupleFactoryJb;
 import swp_compiler_ss13.javabite.quadtruple.QuadrupleJB;
 
 public class PrintNodeConverter extends CastingAst2CodeConverter{
@@ -23,38 +25,32 @@ public class PrintNodeConverter extends CastingAst2CodeConverter{
         PrintNode printNode = (PrintNode) node;
         
         icg.processNode(printNode.getRightValue());
-        IdentifierData newId = icg.popIdentifierData();
-        IdentifierData oldId = icg.popIdentifierData();
-    }
+        IdentifierData rightData = icg.popIdentifierData();
+        Type stringType = new Type(Kind.STRING) {
+			
+			@Override
+			public String toString() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public String getTypeName() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+		};
         
-            
-        	// cast is needed -> its arithmetic so one has to be double and one long
-            // we always upcast to double
-            
-            //rightData = cast(Types., rightData);
-            
-        	public Quadruple printCast(IdentifierData newId, IdentifierData oldId) throws IntermediateCodeGeneratorException {
-                if (newId.getType().getKind() == Kind.LONG) {
-                    switch (oldId.getType().getKind()) {
-                    case DOUBLE:
-                        return new QuadrupleJB(Operator.LONG_TO_DOUBLE, oldId.getIdentifier(), Quadruple.EmptyArgument, newId.getIdentifier());
-                    default:
-                        break;
-                    }
-                } else if (newId.getType().getKind() == Kind.DOUBLE) {
-                    switch (oldId.getType().getKind()) {
-                    case LONG:
-                        return new QuadrupleJB(Operator.DOUBLE_TO_LONG, oldId.getIdentifier(), Quadruple.EmptyArgument, newId.getIdentifier());
-                    default:
-                        break;
-                    }
-                } 
-                    
-                throw new IntermediateCodeGeneratorException("Unsupported cast request for types: " + oldId.getType().getKind() + " -> " + newId.getType().getKind());
-            }
-        	
-      //  }
-   // }
+        // check if types matches
+ 		if (rightData.getType().getKind() != Kind.STRING) {
+ 			// cast is needed -> its arithmetic so one has to be double and one long
+ 			// we always upcast to string
+			rightData = cast(stringType, rightData);
+ 		}
+
+ 		IdentifierData newData = icg.generateTempIdentifier(rightData.getType());
+      //  icg.addQuadruple(QuadrupleFactoryJb.generateRelation(binaryNode.getOperator(), leftData, rightData, newData));
+    }
     
     
     @Override
