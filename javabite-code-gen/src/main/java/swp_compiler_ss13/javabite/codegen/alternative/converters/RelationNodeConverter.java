fbdac2452c0e3471e8d6d1e8cf81f7a4f8ce61df
alpinster <alpinms@yahoo.de>
2013-05-30 15:37:35 +0200

    casting for relation

--- javabite-code-gen/src/main/java/swp_compiler_ss13/javabite/codegen/alternative/converters/RelationNodeConverter.java
+++ javabite-code-gen/src/main/java/swp_compiler_ss13/javabite/codegen/alternative/converters/RelationNodeConverter.java
@@ -4,6 +4,7 @@ import swp_compiler_ss13.common.ast.ASTNode;
 import swp_compiler_ss13.common.ast.ASTNode.ASTNodeType;
 import swp_compiler_ss13.common.ast.nodes.binary.RelationExpressionNode;
 import swp_compiler_ss13.common.ir.IntermediateCodeGeneratorException;
+import swp_compiler_ss13.common.types.Type.Kind;
 import swp_compiler_ss13.javabite.codegen.alternative.CastingAst2CodeConverter;
 import swp_compiler_ss13.javabite.codegen.alternative.IdentifierData;
 import swp_compiler_ss13.javabite.codegen.alternative.QuadrupleFactoryJb;
@@ -22,6 +23,16 @@ public class RelationNodeConverter extends CastingAst2CodeConverter {
         icg.processNode(binaryNode.getRightValue());
         IdentifierData rightData = icg.popIdentifierData();
         
+        // check if types matches
+ 		if (leftData.getType().getKind() != rightData.getType().getKind()) {
+ 			// cast is needed -> its arithmetic so one has to be double and one long
+ 			// we always upcast to double
+ 			if (leftData.getType().getKind() == Kind.LONG) {
+ 				leftData = cast(rightData.getType(), leftData);
+ 			} else {
+ 				rightData = cast(leftData.getType(), rightData);
+ 			}
+ 		}
         
         IdentifierData newData = icg.generateTempIdentifier(leftData.getType());
         icg.addQuadruple(QuadrupleFactoryJb.generateRelation(binaryNode.getOperator(), leftData, rightData, newData));
