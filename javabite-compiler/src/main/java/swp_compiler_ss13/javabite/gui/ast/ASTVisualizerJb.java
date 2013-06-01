2387e2eeeefddbd340320c9a679abd65254e8133
Khaled <khaled3esa@gmail.com>
2013-06-01 15:16:50 +0200

    modifed size of frame

--- javabite-compiler/src/main/java/swp_compiler_ss13/javabite/gui/ast/ASTVisualizerJb.java
+++ javabite-compiler/src/main/java/swp_compiler_ss13/javabite/gui/ast/ASTVisualizerJb.java
@@ -49,6 +49,7 @@ public class ASTVisualizerJb implements ASTVisualization {
 	public mxGraph graph;
 	JScrollPane frame;
 	Queue<Object> toVisit_celledCopy;
+	int x,y;
 	
 	
 	String[] operation={"ADDITION","SUBSTRACTION","MULTIPLICATION",
@@ -76,8 +77,8 @@ public class ASTVisualizerJb implements ASTVisualization {
 
 		initTree(ast);
 		KhaledGraphFrame k= new KhaledGraphFrame();
-		System.out.println(k.levelsCounter(ast));
-		System.out.println(k.maximumOfNodesInLevels());
+		this.x=162*k.levelsCounter(ast);
+		this.y=44*k.maximumOfNodesInLevels();
 		mxHierarchicalLayout layout = new mxHierarchicalLayout(graph);
 		layout.setOrientation(SwingConstants.WEST);
 		layout.setInterRankCellSpacing(80);
@@ -260,7 +261,7 @@ public class ASTVisualizerJb implements ASTVisualization {
 		JFrame frame=new JFrame();
 		JScrollPane ast_frame=visualizer.frame;
 		frame.setVisible(true);
-		frame.setSize(900, 400);
+		frame.setSize(visualizer.x, visualizer.y);
 		frame.add(ast_frame);
 		frame.setVisible(true);
 		
