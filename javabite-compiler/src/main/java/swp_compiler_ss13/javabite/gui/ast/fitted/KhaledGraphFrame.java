31786e1a540177a6fe1533970c6253c6eaf7f10e
Khaled <khaled3esa@gmail.com>
2013-06-01 13:31:36 +0200

    counted levels of ast

--- javabite-compiler/src/main/java/swp_compiler_ss13/javabite/gui/ast/fitted/KhaledGraphFrame.java
+++ javabite-compiler/src/main/java/swp_compiler_ss13/javabite/gui/ast/fitted/KhaledGraphFrame.java
@@ -1,9 +1,13 @@
 package swp_compiler_ss13.javabite.gui.ast.fitted;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 import swp_compiler_ss13.common.ast.AST;
+import swp_compiler_ss13.common.ast.ASTNode;
 import swp_compiler_ss13.javabite.ast.ASTSource;
 import swp_compiler_ss13.javabite.gui.ast.ASTVisualizerJb;
 
@@ -21,6 +25,29 @@ public class KhaledGraphFrame extends GraphFrame {
 		this.setVisible(true);
 	}
 	
+	public void levelsCounter(AST ast){
+		int counter=1;
+		Queue<ASTNode> nextLevel= new ArrayDeque<>();
+		Queue<Queue<ASTNode>> queue= new ArrayDeque<>();
+		Queue<ASTNode> level= new ArrayDeque<>();
+		level.add(ast.getRootNode());
+		queue.add(level);
+		while(counter!=ast.getNumberOfNodes()){
+			for (ASTNode node:level){
+				for (ASTNode child : node.getChildren()){
+					nextLevel.add(child);
+					counter++;
+					}
+				}
+			queue.add(nextLevel);
+			level.clear();
+			for(ASTNode node:nextLevel ){
+				level.add(node);
+			}
+			nextLevel.clear();
+			}
+		System.out.println(queue.size());
+		}
 	public static void main(String[] args){
 		AST ast=ASTSource.getSecondAST();
 		ASTVisualizerJb astViz=new ASTVisualizerJb();
