547e894b6c9e0fa4c5b7df0f4b7c17c50f03dbf3
flofreud <florian.freudenberg@mangirkart.com>
2013-06-09 22:09:31 +0200

    add todos to SettingsPanel

--- javabite-compiler/src/main/java/swp_compiler_ss13/javabite/gui/config/SettingsPanel.java
+++ javabite-compiler/src/main/java/swp_compiler_ss13/javabite/gui/config/SettingsPanel.java
@@ -19,6 +19,8 @@ import javax.swing.event.ListSelectionListener;
 import swp_compiler_ss13.javabite.config.ConfigCategory;
 import swp_compiler_ss13.javabite.config.ConfigKey;
 import swp_compiler_ss13.javabite.config.JavabiteConfig;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class SettingsPanel extends JFrame {
 	final JavabiteConfig config = JavabiteConfig.getDefaultConfig();
@@ -41,9 +43,26 @@ public class SettingsPanel extends JFrame {
 		panel.add(buttonPanel, BorderLayout.SOUTH);
 		
 		JButton btnSave = new JButton("Save");
+		btnSave.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				//TODO: save the changes done on this view
+				//therefore you have to do the following:
+				// * iterate over the formInputs
+				// * check if formInput is changed (isChanged())
+				// * if changed -> insert into config
+				//    -> config.setProperty(key,value)
+			}
+		});
 		buttonPanel.add(btnSave);
 		
 		JButton btnClose = new JButton("Close");
+		btnClose.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				//TODO: close the SettingsPanel
+			}
+		});
 		buttonPanel.add(btnClose);
 		
 		final JPanel formPanel = new JPanel();
