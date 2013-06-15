9a0d68c34e57cabf3729c59989d1ee42cb3c65e1
Marco Wallisch-Prinz <marco@wallisch-prinz.de>
2013-06-15 15:46:30 +0200

    - compiler tests: types

---
+++ javabite-compiler/src/test/java/swp_compiler_ss13/javabite/compiler/jb/types/ErrorBooleanAssignedToStringArrayTest.java
@@ -0,0 +1,34 @@
+package swp_compiler_ss13.javabite.compiler.jb.types;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import swp_compiler_ss13.common.report.ReportType;
+import swp_compiler_ss13.javabite.compiler.AbstractCompilerTest;
+
+public class ErrorBooleanAssignedToStringArrayTest extends AbstractCompilerTest {
+
+	@Override
+	public String getProgFile() {
+		return "jb" + File.separator + "types" + File.separator + "error_type_mismatch_array_decl_assign_string_boolean.prog";
+	}
+
+	@Override
+	public String getOutput() {
+		return "";
+	}
+
+	@Override
+	public Integer getResultValue() {
+		return null;
+	}
+
+	@Override
+	public List<ReportType> getErrorList() {
+		List<ReportType> errors = new ArrayList<>();
+		errors.add(ReportType.TYPE_MISMATCH);
+		return errors;
+	}
+
+}
