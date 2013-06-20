04b9f63794461b0a5c7fb983ef0a5738672a1984
Tilman Krauß <Tilman.krauss@gmail.com>
2013-06-20 23:12:50 +0200

    parser: seems to be ms3-ready ( except the semantic checks)

--- javabite-common/src/main/java/swp_compiler_ss13/javabite/ast/nodes/binary/LoopNodeJb.java
+++ javabite-common/src/main/java/swp_compiler_ss13/javabite/ast/nodes/binary/LoopNodeJb.java
@@ -1,6 +1,7 @@
 package swp_compiler_ss13.javabite.ast.nodes.binary;
 
 import swp_compiler_ss13.common.ast.nodes.ExpressionNode;
+import swp_compiler_ss13.common.ast.nodes.StatementNode;
 import swp_compiler_ss13.common.ast.nodes.binary.LoopNode;
 import swp_compiler_ss13.common.ast.nodes.marynary.BlockNode;
 import swp_compiler_ss13.javabite.ast.nodes.StatementNodeJb;
@@ -9,21 +10,43 @@ public abstract class LoopNodeJb extends StatementNodeJb implements LoopNode {
 	public LoopNodeJb(ASTNodeType myType) {
 		super(myType);
 	}
+	
 	protected ExpressionNode condition;
-	protected BlockNode loopBody;
+	protected StatementNode loopBody;
+	
+	@Deprecated
+	protected BlockNode loopBody_depr;
 	
 	public ExpressionNode getCondition() {
 		return condition;
 	}
+	
+	@Deprecated
+	/**
+	 * Is going to return StatementNode in the future
+	 */
 	public BlockNode getLoopBody() {
-		return loopBody;
+		return loopBody_depr;
 	}
 	
 	public void setCondition(ExpressionNode condition) {
 		this.condition = condition;
 	}
+	
+	public void setLoopBody_(StatementNodeJb body){
+		this.loopBody=body;
+	}
+	
+	public StatementNode getLoopBody_(){
+		return loopBody;
+	}
+	
+	@Deprecated
+	/**
+	 * Is going to receive a StatementNode in the future
+	 */
 	public void setLoopBody(BlockNode loopBody) {
-		this.loopBody = loopBody;
+		this.loopBody_depr = loopBody;
 	}
 	
 	
