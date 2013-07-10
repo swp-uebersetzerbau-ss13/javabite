7cb19b9c9e02091164fc1bb6a95a38b49d4f5c0c
flofreud <florian.freudenberg@fu-berlin.de>
2013-07-10 23:03:30 +0200

    bugfix

--- javabite-code-gen/src/main/java/swp_compiler_ss13/javabite/codegen/CastingAst2CodeConverter.java
+++ javabite-code-gen/src/main/java/swp_compiler_ss13/javabite/codegen/CastingAst2CodeConverter.java
@@ -7,7 +7,7 @@ public abstract class CastingAst2CodeConverter extends AbstractAst2CodeConverter
 	protected IdentifierData cast(Type goalType, IdentifierData oldId) throws IntermediateCodeGeneratorException {
 		if (oldId.getIdentifier().startsWith("#")) {
 			IdentifierData tmpId = icg.generateTempIdentifier(oldId.getType());
-			icg.addQuadruple(QuadrupleFactoryJb.generateAssignment(oldId, tmpId));
+			icg.addQuadruple(QuadrupleFactoryJb.generateAssignment(tmpId, oldId));
 			oldId = tmpId;
 		}
 		IdentifierData newId = icg.generateTempIdentifier(goalType);
